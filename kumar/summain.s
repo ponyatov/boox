d399e030bc083391db5b93c712798f0669cf710e
Dmitry Ponyatov <dponyatov@gmail.com>
2016-03-06 13:11:12 +0400

    + kumar/summain
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ kumar/summain.s
@@ -0,0 +1,13 @@
+        .text
+        b start                 @ пропустить данные
+arr:    .byte 10, 20, 25        @ константный байтовый массив
+eoa:                            @ адрес массива +1
+
+        .align
+start:
+        ldr   r0, =arr          @ r0 = &arr
+        ldr   r1, =eoa          @ r1 = &eoa
+
+        bl    sum               @ (вложенный) вызов процедуры
+
+stop:   b stop
