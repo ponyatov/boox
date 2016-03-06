da16f20d6d45b1a8aedd14be38d03a9126b69ff0
Dmitry Ponyatov <dponyatov@gmail.com>
2016-03-06 22:35:28 +0400

    + summain.s sumsub.s
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ kumar/summain.s
@@ -0,0 +1,10 @@
+        .text
+        b start                 @ пропустить данные
+arr:    .byte 10, 20, 25        @ константный массив байт
+eoa:                            @ адрес массива + 1
+        .align
+start:
+        ldr   r0, =arr          @ r0 = &arr
+        ldr   r1, =eoa          @ r1 = &eoa
+        bl    sum               @ вызвать процедуру
+stop:   b stop
