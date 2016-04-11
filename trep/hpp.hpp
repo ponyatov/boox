74c1658258aca967ff7c370e155e26fbbf39bc81
Dmitry Ponyatov <dponyatov@gmail.com>
2016-04-12 00:03:48 +0400

    + trep
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ trep/hpp.hpp
@@ -0,0 +1,30 @@
+#ifndef _H_TREP
+#define _H_TREP
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Sym {
+	string tag,val;
+	Sym(string,string); Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	virtual string tagval(); string tagstr(); string pad(int);
+	string dump(int=0);
+};
+
+struct List: Sym { List(); };
+
+struct Op: Sym { Op(string); };
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+extern int yyparse();
+extern void yyerror(string);
+#include "ypp.tab.hpp"
+
+#endif // _H_TREP
