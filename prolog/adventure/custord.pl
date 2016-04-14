7f5c8e63b187d78ad75666e88b427f2666850dd8
Dmitry Ponyatov <dponyatov@gmail.com>
2016-04-14 15:06:38 +0400

    v prolog/adv facts
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- prolog/adventure/custord.pl
+++ prolog/adventure/custord.pl
@@ -1,2 +1,19 @@
 /* customer order entry application */
 
+% customer(name,city,credit_rating)
+
+customer(ivanov,moscow,a).
+customer(petrov,spb,b).
+customer(sidorov,samara,bomj).
+
+% sell item(id,name,reorder_point)
+
+item(1,spoon,2).
+item(2,fork,11).
+item(33,'КР142ЕН5А',3).
+
+% stock(id,amount)
+
+stock(1,11).
+stock(2,22).
+stock(33,0).
--- prolog/adventure/gene.pl
+++ prolog/adventure/gene.pl
@@ -1,2 +1,15 @@
 /* genealogical intelligent logicbase */
 
+% gender
+
+male(dennis).
+male(michael).
+
+female(diana).
+
+male('Ghenghis Khan').
+
+% parent relations
+
+parent(dennis, michael).
+parent(dennis, diana).
